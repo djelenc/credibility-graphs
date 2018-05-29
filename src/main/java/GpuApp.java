f49c79770bd82be31b2bd321acfcd0ed7357b5ba
David Jelenc <david.jelenc@fri.uni-lj.si>
2018-05-29 10:47:37 +0200

    Fixes GPU example

--- src/main/java/GpuApp.java
+++ src/main/java/GpuApp.java
@@ -3,27 +3,17 @@ import com.aparapi.Range;
 
 import java.util.Arrays;
 
-final class Example {
-    float val() {
-        return 1f;
-    }
-}
-
 public class GpuApp {
     public static void main(String[] args) {
-        final float data[] = new float[3];
-        // final float inB[] = {3, 4, 5};
-        final Example[] randoms = new Example[data.length];
-        for (int i = 0; i < randoms.length; i++) {
-            randoms[i] = new Example();
-        }
-        final float result[] = new float[data.length];
+        final float data1[] = {1, 2, 3};
+        final float data2[] = {3, 4, 5};
+        final float result[] = new float[data1.length];
 
         final Kernel kernel = new Kernel() {
             @Override
             public void run() {
                 int i = getGlobalId();
-                result[i] = data[i] + randoms[i].val();
+                result[i] = data1[i] + data2[i];
             }
         };
 
