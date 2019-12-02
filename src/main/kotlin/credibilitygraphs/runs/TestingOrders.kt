47c7ea3a200bebdacfba29319401a7b4b169f3e7
David Jelenc <david.jelenc@fri.uni-lj.si>
2019-12-02 23:15:04 +0100

    Adds experiments for all scenarios

--- src/main/kotlin/credibilitygraphs/runs/TestingOrders.kt
+++ src/main/kotlin/credibilitygraphs/runs/TestingOrders.kt
@@ -1,15 +1,17 @@
 package credibilitygraphs.runs
 
+import atb.deceptionmodel.Complementary
 import atb.deceptionmodel.RandomDeception
 import atb.deceptionmodel.Truthful
 import atb.metric.KendallsTauA
 import atb.scenario.Random
+import atb.scenario.RandomWithNewcomers
+import atb.scenario.Transitive
+import atb.scenario.TransitiveWithChanges
 import credibilitygraphs.ProgrammaticRun.Parameters
 import credibilitygraphs.ProgrammaticRun.Setup
 import credibilitygraphs.ProgrammaticRun.run
-import credibilitygraphs.model.Orders
-import credibilitygraphs.model.OrdersRandom
-import credibilitygraphs.model.OrdersReverse
+import credibilitygraphs.model.*
 import java.io.File
 
 
@@ -26,51 +28,51 @@ fun main() {
             OrdersRandom() to emptyArray()
     )
 
-    val fromSeed = 3
+    val fromSeed = 1
     val toSeed = 30
 
     val scenarios = mapOf(
             // simple scenarios
             Random() to Setup(100, fromSeed, toSeed, mapOf(
                     // random truth
-//                    "random-truth-1-9" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.1, Truthful() to 0.9), 0.0, 0.0, 0.1),
-                    "random-truth-9-1" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.9, Truthful() to 0.1), 0.0, 0.0, 0.1)
-//                    "random-truth-5-5" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.5, Truthful() to 0.5), 0.0, 0.0, 0.1),
-//                    // random strict
-//                    "random-strict-1-9" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.1, Strict() to 0.9), 0.0, 0.0, 0.1),
-//                    "random-strict-9-1" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.9, Strict() to 0.1), 0.0, 0.0, 0.1),
-//                    "random-strict-5-5" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.5, Strict() to 0.5), 0.0, 0.0, 0.1),
-//                    // random loose
-//                    "random-loose-1-9" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.1, Loose() to 0.9), 0.0, 0.0, 0.1),
-//                    "random-loose-5-5" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.5, Loose() to 0.5), 0.0, 0.0, 0.1),
-//                    "random-loose-9-1" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.9, Loose() to 0.1), 0.0, 0.0, 0.1),
-//                    // complementary truth
-//                    "complementary-truth-1-9" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.1, Truthful() to 0.9), 0.0, 0.0, 0.1),
-//                    "complementary-truth-9-1" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.9, Truthful() to 0.1), 0.0, 0.0, 0.1),
-//                    "complementary-truth-5-5" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.5, Truthful() to 0.5), 0.0, 0.0, 0.1),
-//                    // complementary strict
-//                    "complementary-strict-1-9" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.1, Strict() to 0.9), 0.0, 0.0, 0.1),
-//                    "complementary-strict-9-1" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.9, Strict() to 0.1), 0.0, 0.0, 0.1),
-//                    "complementary-strict-5-5" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.5, Strict() to 0.5), 0.0, 0.0, 0.1),
-//                    // complementary loose
-//                    "complementary-loose-1-9" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.1, Loose() to 0.9), 0.0, 0.0, 0.1),
-//                    "complementary-loose-9-1" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.9, Loose() to 0.1), 0.0, 0.0, 0.1),
-//                    "complementary-loose-5-5" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.5, Loose() to 0.5), 0.0, 0.0, 0.1)
-            ), mapOf(KendallsTauA() to emptyArray()))
+                    "random-truth-1-9" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.1, Truthful() to 0.9), 0.0, 0.0, 0.1),
+                    "random-truth-9-1" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.9, Truthful() to 0.1), 0.0, 0.0, 0.1),
+                    "random-truth-5-5" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.5, Truthful() to 0.5), 0.0, 0.0, 0.1),
+                    // random strict
+                    "random-strict-1-9" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.1, Strict() to 0.9), 0.0, 0.0, 0.1),
+                    "random-strict-9-1" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.9, Strict() to 0.1), 0.0, 0.0, 0.1),
+                    "random-strict-5-5" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.5, Strict() to 0.5), 0.0, 0.0, 0.1),
+                    // random loose
+                    "random-loose-1-9" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.1, Loose() to 0.9), 0.0, 0.0, 0.1),
+                    "random-loose-5-5" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.5, Loose() to 0.5), 0.0, 0.0, 0.1),
+                    "random-loose-9-1" to arrayOf(100, 0.1, 0.05, mapOf(RandomDeception() to 0.9, Loose() to 0.1), 0.0, 0.0, 0.1),
+                    // complementary truth
+                    "complementary-truth-1-9" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.1, Truthful() to 0.9), 0.0, 0.0, 0.1),
+                    "complementary-truth-9-1" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.9, Truthful() to 0.1), 0.0, 0.0, 0.1),
+                    "complementary-truth-5-5" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.5, Truthful() to 0.5), 0.0, 0.0, 0.1),
+                    // complementary strict
+                    "complementary-strict-1-9" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.1, Strict() to 0.9), 0.0, 0.0, 0.1),
+                    "complementary-strict-9-1" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.9, Strict() to 0.1), 0.0, 0.0, 0.1),
+                    "complementary-strict-5-5" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.5, Strict() to 0.5), 0.0, 0.0, 0.1),
+                    // complementary loose
+                    "complementary-loose-1-9" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.1, Loose() to 0.9), 0.0, 0.0, 0.1),
+                    "complementary-loose-9-1" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.9, Loose() to 0.1), 0.0, 0.0, 0.1),
+                    "complementary-loose-5-5" to arrayOf(100, 0.1, 0.05, mapOf(Complementary() to 0.5, Loose() to 0.5), 0.0, 0.0, 0.1)
+            ), mapOf(KendallsTauA() to emptyArray())),
             // transitive
-//            Transitive() to Setup(100, fromSeed, toSeed, mapOf(
-//                    "tra" to arrayOf(100, 0.1, 0.05, 0.1, 1.0)),
-//                    mapOf(KendallsTauA() to emptyArray())
-//            ),
-//            // transitive with changes
-//            TransitiveWithChanges() to Setup(1000, fromSeed, toSeed, mapOf(
-//                    "dynamic" to arrayOf(100, 0.1, 0.05, 0.1, 1.0, 0.01, 1)),
-//                    mapOf(KendallsTauA() to emptyArray())
-//            ),
-//            // newcomers
-//            RandomWithNewcomers() to Setup(200, fromSeed, toSeed, mapOf(
-//                    "nc" to arrayOf(10, 0.1, 0.05, mapOf(Truthful() to 0.5, Complementary() to 0.5), 0.0, 0.0, 0.5, 10, 5)
-//            ), mapOf(KendallsTauA() to emptyArray()))
+            Transitive() to Setup(100, fromSeed, toSeed, mapOf(
+                    "tra" to arrayOf(100, 0.1, 0.05, 0.1, 1.0)),
+                    mapOf(KendallsTauA() to emptyArray())
+            ),
+            // transitive with changes
+            TransitiveWithChanges() to Setup(1000, fromSeed, toSeed, mapOf(
+                    "dynamic" to arrayOf(100, 0.1, 0.05, 0.1, 1.0, 0.01, 1)),
+                    mapOf(KendallsTauA() to emptyArray())
+            ),
+            // newcomers
+            RandomWithNewcomers() to Setup(200, fromSeed, toSeed, mapOf(
+                    "nc" to arrayOf(10, 0.1, 0.05, mapOf(Truthful() to 0.5, Complementary() to 0.5), 0.0, 0.0, 0.5, 10, 5)
+            ), mapOf(KendallsTauA() to emptyArray()))
     )
 
     val configurations = mutableListOf<Parameters>()
